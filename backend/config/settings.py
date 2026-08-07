00de973214cba3d8f6c87673a366dfc9a74e2aae
Harshitjatav45 <harshitjatav8180@gmail.com>
2026-08-07 20:15:06 +0530

    Configure project to use PostgreSQL

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -89,11 +89,14 @@ AUTH_USER_MODEL = 'accounts.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'distributor_management_system',
+        'USER': 'postgres',
+        'PASSWORD': 'harshit@',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
